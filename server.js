a4968c9fd8f1cefa7ef0ff5f92691022d8232360
unknown <scottypipz@gmail.com>
2019-02-22 16:01:06 +0800

    server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
+
+/*HTTP SERVER*/
+const http = require('http')
+const app = require('./src/api/server/app')
+const server = http.createServer(app)
+const serverPort = 3000;
+server.listen(serverPort)
+console.log('server started ' + serverPort)
+app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
 console.log('server started '+ port);
