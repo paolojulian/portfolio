d7792b2be463938fe3d0933f714d25a08966b8d8
unknown <scottypipz@gmail.com>
2019-02-23 16:14:19 +0800

    test serverless

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
+const express = require('express')
+const app = express()
 
 /*HTTP SERVER*/
-const http = require('http')
-const app = require('./src/api/server/app')
-const server = http.createServer(app)
-const serverPort = 3000;
-server.listen(serverPort, () => {
-    console.log('server started ' + serverPort)
-})
+// const http = require('http')
+// const app = require('./src/api/server/app')
+// const server = http.createServer(app)
+// const serverPort = 3000;
+// server.listen(serverPort, () => {
+//     console.log('server started ' + serverPort)
+// })
 app.use(serveStatic(__dirname + "/dist"));
 app.listen(port, () => {
     console.log('server started '+ port);
