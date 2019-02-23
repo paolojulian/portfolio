7ca55112128871bb56508a74176d0054893d82aa
unknown <scottypipz@gmail.com>
2019-02-23 11:35:19 +0800

    sql credentials

--- src/api/server/sql.js
+++ src/api/server/sql.js
@@ -1,12 +1,13 @@
+//mysql://ba30a4bf0efd95:621f9c8c@us-cdbr-iron-east-03.cleardb.net/heroku_d9838c7ffb34abb?reconnect=true
 const mysql = require('mysql')
 
 /** MYSQL */
 // Connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'portfolio'
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'ba30a4bf0efd95',
+    password: '621f9c8c',
+    database: 'heroku_d9838c7ffb34abb'
 });
 // Connect DB
 db.connect((err) => {
