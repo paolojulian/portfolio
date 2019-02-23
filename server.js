ba75c47bd5e7f71d4b0e45be761783ffba122b15
unknown <scottypipz@gmail.com>
2019-02-23 17:15:40 +0800

    server

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const serveStatic = require('serve-static');
 const http = require('http')
 const app = require('./src/api/server/app')
 const port = process.env.PORT || 5000;
-// const fs = require('fs')
-const server = http.createServer(app)
-app.use(serveStatic(__dirname + "/dist"));
-// const express = require('express')
-// const app = express()
-// app.use((req, res) => {
-//     fs.readFile('public/index.html', 'utf-8', (err, content) => {
-//         if (err) {
-//             console.log('We cannot open "index.html" file.')
-//         }
+const fs = require('fs');
+const server = http.createServer(app);
+const fileLocation = __dirname + "/dist";
 
-//         res.writeHead(200, {
-//             'Content-Type': 'text/html; charset=utf-8'
-//         })
+app.use(serveStatic(fileLocation));
+app.use((req, res) => {
+    fs.readFile(fileLocation + '/index.html', 'utf-8', (err, content) => {
+        if (err) {
+            console.log('We cannot open "index.html" file.')
+        }
 
-//         res.end(content)
-//     })
-// })
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        res.end(content)
+    })
+})
 
 server.listen(port, () => {
     console.log('server started '+ port);
